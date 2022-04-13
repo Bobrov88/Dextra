bbbf2454a3272cf64ceedfb9dcf718870e475456
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-04-13 16:42:10 +0300

    13.04.2022 16:41

--- STL/main.cpp
+++ STL/main.cpp
@@ -20,9 +20,18 @@ int priority(char ops)
 
 struct Data
 {
-	int number;
+	float number;
 	char symbol;
 	bool is_num;
+	void input(
+		char _number,
+		char _symbol,
+		bool _is_num)
+	{
+		number = (int)_number-48;
+		symbol = _symbol;
+		is_num = _is_num;
+	}
 };
 
 
@@ -30,51 +39,68 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 	string expression;
-	stack<char> Exit;
+	stack<Data> Exit;
 	stack<char> ops;
+	Data element;
 	ops.push(' ');
 	int i = 0;
-	cout << offset<< "Введите выражение: "; cin >> expression;
+	cout << offset << "Введите выражение: "; cin >> expression;
 	cout << offset << "ОПЗ: ";
 	do
-	{
-
-		if ((((int)expression[i] > 47) && ((int)expression[i] < 58)) || (expression[i] == '!'))
+	{//(expression[i] == '!')
+		if (((int)expression[i] > 47) && ((int)expression[i] < 58))
 		{
-			Exit.push(expression[i]);
-			cout << Exit.top();
+			element.input(expression[i], '+', true);
+			i++;
+			while ((int)expression[i] > 47 && (int)expression[i] < 58)
+			{
+				element.number = element.number * 10 + (int)expression[i]-48;
+				i++;
+			}
+			Exit.push(element);
+			cout << Exit.top().number<<" ";
+			element.number = 0;
 		}
 		else if (expression[i] == ')')
-		{ 
+		{
 			while (ops.top() != '(')
 			{
-				Exit.push(ops.top());
+				element.input(0, ops.top(), false);
+				Exit.push(element);
 				ops.pop();
-				cout << Exit.top();
+				cout << Exit.top().symbol<<" ";
 			}
 			ops.pop();
+			i++;
 		}
-		else if (expression[i] == '(')	ops.push(expression[i]);
-		else {
+		else if (expression[i] == '(')
+		{
+			ops.push(expression[i]);
+			i++;
+		}
+		else
+		{
 			if (priority(ops.top()) < priority(expression[i]))	ops.push(expression[i]);
 			else
 			{
 				while (priority(ops.top()) >= priority(expression[i]))
 				{
-					Exit.push(ops.top());
+					element.input(0, ops.top(), false);
+					Exit.push(element);
 					ops.pop();
-					cout << Exit.top();
+					cout << Exit.top().symbol<<" ";
 				}
 				ops.push(expression[i]);
 			}
-			}
-		i++;
+			i++;
+		}
 	} while (i < expression.length());
 	while (ops.top() != ' ')
 	{
-		Exit.push(ops.top());
+		element.input(0, ops.top(), false);
+		Exit.push(element);
 		ops.pop();
-		cout << Exit.top();
+		cout << Exit.top().symbol<<" ";
 	}
 	return 0;
-}	
+}
