edbdf3dc06cafb2e3a81cf645ee679739982225c
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-04-15 16:12:53 +0300

    15.04.2022 15:10

--- STL/main.cpp
+++ STL/main.cpp
@@ -44,7 +44,7 @@ float operation(float a, float b, char op)
 	case '*': return a * b;
 	case '/': return a / b;
 	case '^': return pow(a, b);
-	case '!': return a;
+//	case '!': return factorial(a);
 	default: return 1;
 	}
 }
@@ -60,16 +60,16 @@ int main()
 	Data element;
 	float Temp;
 	ops.push(' ');
-	int i = 0;
+	int i = 0, pow10 = 1;
 	cout << offset << "Введите выражение: "; cin >> expression;
-	cout << offset << "ОПЗ: ";
+	//cout << offset << "ОПЗ: ";
 	do
 	{
 		if (expression[i] == '!')
 		{
 			element.input(0, '!', false);
 			Exit.push(element);
-			cout << Exit.top().symbol<<" ";
+			//	cout << Exit.top().symbol << " ";
 			i++;
 		}
 		else if (((int)expression[i] > 47) && ((int)expression[i] < 58))
@@ -82,8 +82,21 @@ int main()
 				i++;
 			}
 			Exit.push(element);
-			cout << Exit.top().number<<" ";
+			//	cout << Exit.top().number << " ";
 			element.number = 0;
+		}
+		else if ((expression[i] == '.') || (expression[i] == ','))
+		{
+			//cout << expression[i];
+			i++;
+			pow10 = 1;
+			while (((int)expression[i] > 47) && ((int)expression[i] < 58))
+			{
+				Exit.top().number = Exit.top().number + ((int)expression[i] - 48) / pow(10, pow10);
+				//		cout << expression[i];
+				pow10++;
+				i++;
+			}
 		}
 		else if (expression[i] == ')')
 		{
@@ -92,7 +105,7 @@ int main()
 				element.input(0, ops.top(), false);
 				Exit.push(element);
 				ops.pop();
-				cout << Exit.top().symbol<<" ";
+				//		cout << Exit.top().symbol << " ";
 			}
 			ops.pop();
 			i++;
@@ -112,7 +125,7 @@ int main()
 					element.input(0, ops.top(), false);
 					Exit.push(element);
 					ops.pop();
-					cout << Exit.top().symbol<<" ";
+					//			cout << Exit.top().symbol << " ";
 				}
 				ops.push(expression[i]);
 			}
@@ -124,13 +137,16 @@ int main()
 		element.input(0, ops.top(), false);
 		Exit.push(element);
 		ops.pop();
-		cout << Exit.top().symbol;
+		//		cout << Exit.top().symbol;
 	}
-	cout << endl;
+//	cout << endl;
 	do
 	{
 		Reverse.push(Exit.top());
 		Exit.pop();
+		//if (Reverse.top().is_num) cout << Reverse.top().number;
+		//else cout << Reverse.top().symbol;
+		//cout << " ";
 	} while (!Exit.empty());
 
 	do
@@ -138,13 +154,20 @@ int main()
 		while (Reverse.top().is_num)
 		{
 			Exit.push(Reverse.top());
+			Reverse.pop();//ошибка если в выражении всего один элемент
+		}
+		if (Reverse.top().symbol == '!')
+		{
+			Exit.top().number = operation(Exit.top().number, 1, '!');
+		}
+		else
+		{
+			Temp = Exit.top().number;
+			Exit.pop();
+			Exit.top().number = operation(Exit.top().number, Temp, Reverse.top().symbol);
 			Reverse.pop();
 		}
-		Temp = Exit.top().number;
-		Exit.pop();
-		Exit.top().number = operation(Exit.top().number, Temp, Reverse.top().symbol);
-		Reverse.pop();
 	} while (!Reverse.empty());
-	cout << "\n" << offset << "Ответ: " << Exit.top().number;
+	cout << offset << "Ответ: " << Exit.top().number;
 	return 0;
 }
