e268cdc569d72cd28ae5b9704c035d5e6c062a40
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-05-17 15:33:28 +0300

    2

---
+++ AUTO/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	auto a = 10;
+	auto b = 7.8;
+	auto c = true;
+	auto d = "ggf";
+	auto e = 'c';
+	auto z = b;
+	return 0;
+}
