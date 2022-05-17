933b250645d011a97b58e3b0e85137ab65bc47b4
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-05-17 16:43:45 +0300

    3

--- VECTOR/main.cpp
+++ VECTOR/main.cpp
@@ -5,8 +5,42 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	vector<int> myVector(20, 3);
-		//= { 0,484,484,999 };
+	vector<int> myVector{ 1, 9,44,422,676,78 };
+
+	//for (vector<int>::iterator i=myVector.begin(); i != myVector.end(); i++)
+	//{
+	//	cout << *i << endl;
+	//}
+	//cout << "INSERT" << endl;
+	//vector<int>::iterator it = myVector.begin();
+	//advance(it, 3);
+	//myVector.insert(it,999);
+	//for (it = myVector.begin(); it != myVector.end(); it++)
+	//{
+	//	cout << *it << endl;
+	//}
+	//cout << "ERASE+3" << endl;
+	//vector<int>::iterator itErase = myVector.begin();
+	//myVector.erase(itErase,itErase+3);
+	//for (it = myVector.begin(); it != myVector.end(); it++)
+	//{
+	//	cout << *it << endl;
+	//}
+	//for (vector<int>::const_iterator i = myVector.cbegin(); i != myVector.cend(); i++)
+	//{
+	//	cout << *i << endl;
+	//}
+	/*for (vector<int>::reverse_iterator i = myVector.rbegin(); i != myVector.rend(); i++)
+	{
+		cout << *i << endl;
+	}*/
+	//cout << *it << endl;
+	//advance(it, 3);
+	//cout << *it << endl;
+
+	//{
+	//	cout << *i << endl;
+	//}
 	/*myVector.reserve(100);
 	myVector.push_back(2);
 	myVector.push_back(44);
@@ -23,27 +57,27 @@ int main()
 	//{
 	//	cout << ex.what()<<endl;
 	//}
-	cout << "Number: " << myVector.size() << endl;
-	for (int i = 0; i < myVector.size(); i++)
-	{
-		cout << myVector[i] << endl;
-	}
-	//	myVector.clear();
-	cout << "Capacity " << myVector.capacity() << endl;
-	myVector.pop_back();
-	cout << "Number: " << myVector.size() << endl;
-	for (int i = 0; i < myVector.size(); i++)
-	{
-		cout << myVector[i] << endl;
-	}
-	myVector.shrink_to_fit();
-	cout << "Capacity " << myVector.capacity() << endl;
-	cout << "Empty " << myVector.empty()<<endl;
-	myVector.resize(4);
-	for (int i = 0; i < myVector.size(); i++)
-	{
-		cout << myVector[i] << endl;
-	}
-	cout << "Capacity " << myVector.capacity() << endl;
+	//cout << "Number: " << myVector.size() << endl;
+	//for (int i = 0; i < myVector.size(); i++)
+	//{
+	//	cout << myVector[i] << endl;
+	//}
+	////	myVector.clear();
+	//cout << "Capacity " << myVector.capacity() << endl;
+	//myVector.pop_back();
+	//cout << "Number: " << myVector.size() << endl;
+	//for (int i = 0; i < myVector.size(); i++)
+	//{
+	//	cout << myVector[i] << endl;
+	//}
+	//myVector.shrink_to_fit();
+	//cout << "Capacity " << myVector.capacity() << endl;
+	//cout << "Empty " << myVector.empty()<<endl;
+	//myVector.resize(4);
+	//for (int i = 0; i < myVector.size(); i++)
+	//{
+	//	cout << myVector[i] << endl;
+	//}
+	//cout << "Capacity " << myVector.capacity() << endl;
 	return 0;
 }
